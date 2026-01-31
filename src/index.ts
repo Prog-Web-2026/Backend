e3b27c71a70c20bcf6bedd5350d272bed1c01f5a
Lucas de Lima da Silva <lucas.lima.silva@ccc.ufcg.edu.br>
2026-01-30 23:59:54 -0300

    Rate-limit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import sequelize from "./config/database";
+import rateLimit from "express-rate-limit";
 
 // Importando rotas
 import userRoutes from "./routes/UserRoutes";
@@ -22,6 +23,20 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minuto
+  max: 5,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: "Muitas requisições. Tente novamente em instantes."
+});
+app.use(limiter);
+
+app.get("/", (_req, res) => {
+  res.send("API rodando! Acesse suas rotas CRUD via endpoints.");
+});
+
+// ROTAS
 app.use("/users", userRoutes);
 app.use("/clientes", clienteRoutes);
 app.use("/enderecos", enderecoRoutes);
@@ -42,12 +57,10 @@ sequelize
   .sync({ force: false })
   .then(() => {
     console.log("Banco de dados conectado!");
-    app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+    app.listen(PORT, () =>
+      console.log(`Servidor rodando na porta ${PORT}`)
+    );
   })
   .catch((error) => {
     console.error("Erro ao conectar ao banco de dados:", error);
   });
-
-app.get("/", (_req, res) => {
-  res.send("API rodando! Acesse suas rotas CRUD via endpoints.");
-});
